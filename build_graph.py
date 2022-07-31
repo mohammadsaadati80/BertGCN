95630941f7c1d19440bc799ab88e4960033ad2c3
Mohammad Saadati <mohammadsaadati80@yahoo.com>
2022-07-31 10:54:31 +0000

    Add persian dataset name

--- build_graph.py
+++ build_graph.py
@@ -15,7 +15,7 @@ from scipy.spatial.distance import cosine
 if len(sys.argv) != 2:
 	sys.exit("Use: python build_graph.py <dataset>")
 
-datasets = ['20ng', 'R8', 'R52', 'ohsumed', 'mr', 'offEval19a1', 'offEval19a2']
+datasets = ['20ng', 'R8', 'R52', 'ohsumed', 'mr', 'offEval19a1', 'offEval19a2', 'persian']
 # build corpus
 dataset = sys.argv[1]
 
--- finetune_bert.py
+++ finetune_bert.py
@@ -19,7 +19,7 @@ parser.add_argument('--max_length', type=int, default=128, help='the input lengt
 parser.add_argument('--batch_size', type=int, default=128)
 parser.add_argument('--nb_epochs', type=int, default=60)
 parser.add_argument('--bert_lr', type=float, default=1e-4)
-parser.add_argument('--dataset', default='20ng', choices=['20ng', 'R8', 'R52', 'ohsumed', 'mr', 'offEval19a1', 'offEval19a2'])
+parser.add_argument('--dataset', default='20ng', choices=['20ng', 'R8', 'R52', 'ohsumed', 'mr', 'offEval19a1', 'offEval19a2', 'persian'])
 parser.add_argument('--bert_init', type=str, default='roberta-base',
                     choices=['roberta-base', 'roberta-large', 'bert-base-uncased', 'bert-large-uncased'])
 parser.add_argument('--checkpoint_dir', default=None, help='checkpoint directory, [bert_init]_[dataset] if not specified')
